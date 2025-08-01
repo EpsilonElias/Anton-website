bf96efc2a301acdd930acdddd3019c9c434ddac8
Elias Serzhan <EliasSerzhan@gmail.com>
2025-07-31 19:46:09 -0700

    trying to load the stuff

--- therapy-website/src/api.js
+++ therapy-website/src/api.js
@@ -2,4 +2,6 @@ const isLocalhost = window.location.hostname === 'localhost';
 
 export const API_BASE = isLocalhost
   ? 'http://localhost:3000' // Your local Payload server
-  : (process.env.REACT_APP_API_URL || 'https://dr-serzhans-psycare.onrender.com'); // fallback production URL
+  : (process.env.REACT_APP_API_URL 
+    || 'https://wings-ordinary-maximize-movies.trycloudflare.com/'
+    || 'https://dr-serzhans-psycare.onrender.com'); // fallback production URL
