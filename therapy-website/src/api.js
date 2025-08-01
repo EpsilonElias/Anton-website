c88f5a304248d9986e7a4dd88157c1c2dcc8c25e
Elias Serzhan <EliasSerzhan@gmail.com>
2025-07-31 23:10:27 -0700

    updated with new link 4

--- therapy-website/src/api.js
+++ therapy-website/src/api.js
@@ -1,5 +1,1 @@
-const isLocalhost = window.location.hostname === 'localhost';
-
-export const API_BASE = isLocalhost
-  ? 'http://localhost:3000'
-  : process.env.REACT_APP_API_URL || 'https://hungry-equally-lewis-disciplinary.trycloudflare.com/';
+export const API_BASE = 'https://hungry-equally-lewis-disciplinary.trycloudflare.com';
