26d9f7ec96b135f564e3fd78d93f32b942ef8a9f
Elias Serzhan <EliasSerzhan@gmail.com>
2025-07-31 20:37:24 -0700

    updated with new link 2

--- therapy-website/src/api.js
+++ therapy-website/src/api.js
@@ -1,7 +1,5 @@
 const isLocalhost = window.location.hostname === 'localhost';
 
 export const API_BASE = isLocalhost
-  ? 'http://localhost:3000' // Your local Payload server
-  : (process.env.REACT_APP_API_URL 
-    || 'https://spirits-machinery-affiliation-talk.trycloudflare.com/'
-    || 'https://dr-serzhans-psycare.onrender.com'); // fallback production URL
+  ? 'http://localhost:3000'
+  : process.env.REACT_APP_API_URL || 'https://spirits-machinery-affiliation-talk.trycloudflare.com';
