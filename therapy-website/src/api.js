0970fe71f08a91c98398f566a4013f57dba81672
Elias Serzhan <EliasSerzhan@gmail.com>
2025-07-31 22:42:58 -0700

    updated with new link 3

--- therapy-website/src/api.js
+++ therapy-website/src/api.js
@@ -2,4 +2,4 @@ const isLocalhost = window.location.hostname === 'localhost';
 
 export const API_BASE = isLocalhost
   ? 'http://localhost:3000'
-  : process.env.REACT_APP_API_URL || 'https://spirits-machinery-affiliation-talk.trycloudflare.com';
+  : process.env.REACT_APP_API_URL || 'https://hungry-equally-lewis-disciplinary.trycloudflare.com/';
