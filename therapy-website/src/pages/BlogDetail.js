6e4d7d8f2b0a7cbb45ab250919800cbf21be2387
Elias Serzhan <EliasSerzhan@gmail.com>
2025-08-28 16:59:06 -0700

    Remove test component from production BlogDetail page

--- therapy-website/src/pages/BlogDetail.js
+++ therapy-website/src/pages/BlogDetail.js
@@ -4,7 +4,6 @@ import { Undo2 } from "lucide-react";
 import { API_BASE } from '../api';
 import SafeImage from '../components/SafeImage';
 import HtmlContent from '../components/HtmlContent';
-import SimpleImageTest from '../components/SimpleImageTest';
 
 function BlogDetail() {
   const { id } = useParams();
@@ -243,9 +242,6 @@ function BlogDetail() {
           </p>
         )}
       </div>
-
-      {/* Temporary Image Test Component */}
-      <SimpleImageTest />
 
       {/* Blog content */}
       <div style={{ 
